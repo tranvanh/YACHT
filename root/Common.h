cc451ae04d74ad128f3b787cf5f7825835302682
bi <tranvietanhcz@gmail.com>
2024-10-16 01:55:40 +0200

    add todo

--- root/Common.h
+++ root/Common.h
@@ -4,19 +4,19 @@
 #include <memory>
 #include <utility>
 
+
+// \todo FIX THIS SHIT AND MAKE IT WORK
 #ifndef NDEBUG
-#   define CASSERT(Expr, Msg) \
-    IMPL_Assert(#Expr, Expr, __FILE__, __LINE__, Msg)
+#   define CASSERT(Expr, Msg) IMPL_Assert(#   Expr, Expr, __FILE__, __LINE__, Msg)
 #else
 #   define CASSERT(Expr, Msg)
 #endif
 
-static void IMPL_Assert(const char* expr_str, bool expr, const char* file, int line, const char* msg){
-    if (!expr)
-    {
+static void IMPL_Assert(const char* expr_str, bool expr, const char* file, int line, const char* msg) {
+    if (!expr) {
         std::cerr << "Assert failed:\t" << msg << "\n"
-            << "Expected:\t" << expr_str << "\n"
-            << "Source:\t\t" << file << ", line " << line << "\n";
+                  << "Expected:\t" << expr_str << "\n"
+                  << "Source:\t\t" << file << ", line " << line << "\n";
         abort();
     }
 }
